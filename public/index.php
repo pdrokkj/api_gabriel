3a1d051ad188a84cadb1b524ad348b75501bc1b0
pdrokkj <pedro.santos12@alunos.sc.senac.br>
2025-04-23 15:51:05 -0300

    dada

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -8,32 +10,11 @@ use App\Geometria;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->addBodyParsingMiddleware();
 
-require __DIR__ . '/../src/Geometria.php';
-
-// Rota para área do retângulo
 $app->post('/area/retangulo', function (Request $request, Response $response) {
     $dados = $request->getParsedBody();
-    $base = $dados['base'] ?? 0;
-    $altura = $dados['altura'] ?? 0;
-
     $geo = new Geometria();
-    $area = $geo->calcularAreaRetangulo($base, $altura);
-
-    $response->getBody()->write(json_encode(['area' => $area]));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-// Rota para área do triângulo
-$app->post('/area/triangulo', function (Request $request, Response $response) {
-    $dados = $request->getParsedBody();
-    $base = $dados['base'] ?? 0;
-    $altura = $dados['altura'] ?? 0;
-
-    $geo = new Geometria();
-    $area = $geo->calcularAreaTriangulo($base, $altura);
-
+    $area = $geo->calcularAreaRetangulo($dados['base'], $dados['altura']);
     $response->getBody()->write(json_encode(['area' => $area]));
     return $response->withHeader('Content-Type', 'application/json');
 });
--- src/geometria.php
+++ src/geometria.php
@@ -2,15 +2,12 @@
 
 namespace App;
 
-class Geometria
-{
-    public function calcularAreaRetangulo($base, $altura)
-    {
+class Geometria {
+    public function calcularAreaRetangulo($base, $altura) {
         return $base * $altura;
     }
 
-    public function calcularAreaTriangulo($base, $altura)
-    {
+    public function calcularAreaTriangulo($base, $altura) {
         return ($base * $altura) / 2;
     }
 }
